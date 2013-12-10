c0a3ea813151c38071518840b22a41173d31f929
wentianle <wentianle@ucloud.cn>
2013-12-10 16:30:07 +0800

    修改说明

--- config.simple.py
+++ config.simple.py
@@ -1,7 +1,7 @@
 #-*- encoding: utf-8 -*-
 #配置公私钥"""
-public_key  = "ucloudwentianle@ucloud.cn13595660461202471273"
-private_key = "8a78e7bde21c6301d2fc35b56091b159dedbf2e9"
+public_key  = ""
+private_key = ""
 
 #配置api地址'''
 #无外网IP主机使用(ucloud 云主机中)
